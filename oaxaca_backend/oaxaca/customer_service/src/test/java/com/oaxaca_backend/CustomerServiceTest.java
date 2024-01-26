196f90bb40f251e886eddd009b787a39aab008ac
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-01-26 07:36:27 +0000

    add rest

---
+++ oaxaca_backend/oaxaca/customer_service/src/test/java/com/oaxaca_backend/CustomerServiceTest.java
@@ -0,0 +1,34 @@
+package com.oaxaca_backend.customer_service;
+
+import com.oaxaca_backend.customer_service.model.Customer;
+import com.oaxaca_backend.customer_service.repository.CustomerRepository;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceTest {
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @InjectMocks
+    private CustomerService customerService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    
+
+
+
+
+    
+}
