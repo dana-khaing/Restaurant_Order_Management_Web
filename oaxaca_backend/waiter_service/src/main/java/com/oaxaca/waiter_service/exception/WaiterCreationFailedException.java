fce8287679f3c5cd9584064635f976e3c78e79cf
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-01-29 19:31:59 +0000

    fix final bugs

--- oaxaca_backend/waiter_service/src/main/java/com/oaxaca/waiter_service/exception/WaiterCreationFailedException.java
+++
@@ -1,9 +0,0 @@
-package com.oaxaca.waiter_service.exception;
-
-public class WaiterCreationFailedException extends RuntimeException {
-
-    public WaiterCreationFailedException(String message) {
-        super(message);
-    }
-    
-}
