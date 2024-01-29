fd6d2d5255015a21e2434e2e36ee67cc287272f5
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-01-29 19:25:26 +0000

    Fix bug in login functionality

---
+++ oaxaca_backend/customer_service/src/main/java/com/oaxaca/customer_service/controller/CustomerController.java
@@ -0,0 +1,71 @@
+package com.oaxaca.customer_service.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.oaxaca.customer_service.exception.CustomerCreationFailedException;
+import com.oaxaca.customer_service.model.Customer;
+import com.oaxaca.customer_service.service.CustomerService;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> createCustomer(@RequestBody Customer customer) {
+        if (customer == null || customer.getUsername() == null || customer.getUsername().isEmpty()
+                || customer.getPassword() == null || customer.getPassword().isEmpty() || customer.getEmail() == null
+                || customer.getEmail().isEmpty()) {
+            return new ResponseEntity<>("Customer creation failed", HttpStatus.BAD_REQUEST);
+        }
+
+        Customer createdCustomer = customerService.createCustomer(customer);
+        return new ResponseEntity<>(createdCustomer, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/find/id/{id}")
+    public Customer findCustomerById(@PathVariable Long id) {
+        return customerService.findCustomerById(id);
+    }
+
+    @GetMapping("/find/username/{username}")
+    public Customer findCustomerByUsername(@PathVariable String username) {
+        return customerService.findCustomerByUsername(username);
+    }
+
+    @GetMapping("/find/email/{email}")
+    public Customer findCustomerByEmail(@PathVariable String email) {
+        return customerService.findCustomerByEmail(email);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginCustomer(@RequestBody Customer customer) {
+        if (customer == null || customer.getUsername() == null || customer.getUsername().isEmpty()
+                || customer.getPassword() == null || customer.getPassword().isEmpty()) {
+            return new ResponseEntity<>("Customer login failed. Wrong username or password", HttpStatus.BAD_REQUEST);
+        }
+
+        Customer foundCustomer = customerService.findCustomerByUsername(customer.getUsername());
+        if (foundCustomer == null) {
+            return new ResponseEntity<>("Customer login failed", HttpStatus.BAD_REQUEST);
+        }
+
+        if (!foundCustomer.getPassword().equals(customer.getPassword())) {
+            return new ResponseEntity<>("Customer login failed. Wrong username or password", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(foundCustomer, HttpStatus.OK);
+    }
+
+}
