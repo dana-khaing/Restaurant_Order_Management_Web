2c1ec5c9e7df083b5dbc99a5e52d5b536bcce57f
HtetAung99 <htetaung251299@gmail.com>
2024-01-31 18:36:00 +0000

    added appbar in root layout

--- oaxaca/app/page.js
+++ oaxaca/app/page.js
@@ -1,18 +1,17 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <main>
+      <h1>Ready? Oaxaca time!</h1>
+      <p>
+        Oaxaca is a vibrant Mexican restaurant offering an authentic dining
+        experience. Join us for a culinary journey that's bursting with the
+        unique flavors and textures of Mexico.
+      </p>
 
-      <section>
-        <h1>Ready? Oaxaca time!</h1>
-        <p>Oaxaca is a vibrant Mexican restaurant offering an authentic dining experience. Join us for a culinary journey that's bursting with the unique flavors and textures of Mexico.</p>
-
-        <Button href="/login">Login</Button>
-        <Button href="/signup">Sign up</Button>
-
-      </section>
-
+      <Button href='/login'>Login</Button>
+      <Button href='/signup'>Sign up</Button>
     </main>
   );
 }
