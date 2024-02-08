e07bce42a7fc8f2d3d6942f5a3b108d98632fb7d
Irene Vazquez <ZLAC182@live.rhul.ac.uk>
2024-02-08 19:46:35 +0000

    Remove from cart method

--- oaxaca/app/ordering.js
+++ oaxaca/app/ordering.js
@@ -4,6 +4,9 @@ const OrderingUI = () => {
  const [cartItems, setCartItems] = useState([]);
 
  const addToCart = (item) => {
-   setCartItems([...cartItems, item]);
-  };
+    setCartItems([...cartItems, item]);
+    };
+  const removeFromCart = (itemId) => {
+    setCartItems(cartItems.filter((item) => item.id !== itemId));
+    };
 }
