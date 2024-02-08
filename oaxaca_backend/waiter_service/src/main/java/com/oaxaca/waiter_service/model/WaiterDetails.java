3b31ca868793eb71781ec4d543d1f99ee5f9419e
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-02-08 16:49:57 +0000

    Implement Waiter Details class
    In order to allow custom fetching
    of attributes, user details must be implemented using a Waiter Details class
    to allow for fetching of these
    custom attributes

---
+++ oaxaca_backend/waiter_service/src/main/java/com/oaxaca/waiter_service/model/WaiterDetails.java
@@ -0,0 +1,74 @@
+package com.oaxaca.waiter_service.model;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+public class WaiterDetails implements UserDetails {
+
+    private Waiter waiter;
+
+    public WaiterDetails(Waiter waiter) {
+        this.waiter = waiter;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return waiter.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return waiter.getUsername();
+    }
+
+    public String getFirstName() {
+        return waiter.getName();
+    }
+
+    public String getLastName() {
+        return waiter.getLastname();
+    }
+
+    public String getRestaurantAddress() {
+        return waiter.getRestaurantAddress();
+    }
+
+    public String getRestaurantName() {
+        return waiter.getRestaurantName();
+    }
+
+    public String getManagerName() {
+        return waiter.getManagerName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+       return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+}
