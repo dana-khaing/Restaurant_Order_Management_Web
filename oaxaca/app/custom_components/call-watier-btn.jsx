010b9459ed7a509dc4d05ebf1ccdd28db7b4e9e1
Dana Khaing <WLIS410@live.rhul.ac.uk>
2024-02-13 22:46:59 +0000

    implemented toast message popping when the call-waiter button is clicked

--- oaxaca/app/custom_components/call-watier-btn.jsx
+++ oaxaca/app/custom_components/call-watier-btn.jsx
@@ -1,8 +1,16 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
 import React from "react";
 
+const handleClick = () => {
+  toast({
+    title: "Waiter is coming...",
+    description: "Please wait a moment! Our waiter is coming to serve you!",
+  });
+};
+
 export default function CallWaiterButton() {
-  return <Button onClick={() => console.log("hello")}>Call Waiter</Button>;
+  return <Button onClick={handleClick}>Call Waiter</Button>;
 }
