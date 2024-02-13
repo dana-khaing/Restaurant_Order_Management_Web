9ac40f63ba035dba649f9de45a869624f7ef7003
Dana Khaing <WLIS410@live.rhul.ac.uk>
2024-02-13 22:29:58 +0000

    created a feature branch to implement call-waiter functionality and added a button in the navigation bar

--- oaxaca/app/(home)/layout.jsx
+++ oaxaca/app/(home)/layout.jsx
@@ -1,16 +1,16 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function HomeLayout({ children }) {
   return (
     <div>
-      <div className='bg-[#EF3C3C] flex justify-between items-center p-4'>
-        <span className='text-2xl text-white tracking-wider'>OAXACA</span>
-        <div className='flex gap-3 items-center'>
-          <Link className='text-white text-lg font-semibold' href='/'>
+      <div className="bg-[#EF3C3C] flex justify-between items-center p-4">
+        <span className="text-2xl text-white tracking-wider">OAXACA</span>
+        <div className="flex gap-3 items-center">
+          <Button>Call Waiter</Button>
+          <Link className="text-white text-lg font-semibold" href="/">
             Log In
           </Link>
-          <Button>Book a Table</Button>
         </div>
       </div>
       {children}
