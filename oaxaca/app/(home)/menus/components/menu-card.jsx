c50ae7b9a5bd51358c6a9ecc5c694d8a12d094ed
AUNG Htet (2023) WLIS379 <htet.aung.2023@live.rhul.ac.uk>
2024-02-14 14:15:15 +0000

    added fetching functionality in frontend for menus

--- oaxaca/app/(home)/menus/components/menu-card.jsx
+++ oaxaca/app/(home)/menus/components/menu-card.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-function MenuCard({ name, diet }) {
+function MenuCard({ menu: { name, description, price } }) {
   return (
-    <div className='w-52 border border-[#EF3C3C] rounded-xl' key={name}>
+    <div className='w-52 border border-[#EF3C3C] rounded-xl'>
       <img src='/images/burrito.jpeg' className='w-full h-36 rounded-t-xl' />
       <div className='p-2'>
         <h5 className='text-md font-semibold'>{name}</h5>
         <span className='text-xs px-1 bg-green-500 capitalize rounded-md'>
-          {diet}
+          {description}
         </span>
         <button className='bg-[#EF3C3C] text-white rounded-lg px-7 py-1 block mx-auto mt-2'>
           Detail
--- oaxaca/app/(home)/menus/components/menu-list.jsx
+++ oaxaca/app/(home)/menus/components/menu-list.jsx
@@ -1,32 +1,45 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import MenuCard from './menu-card';
 
 function MenuList() {
-  const menuItems = [
-    { name: 'Burrito', diet: 'vegan' },
-    { name: 'Salad', diet: 'vegetarian' },
-    { name: 'Pasta', diet: 'gluten-free' },
-    { name: 'Smoothie', diet: 'vegan' },
-    { name: 'Grilled Chicken', diet: 'protein-rich' },
-    { name: 'Sushi Roll', diet: 'pescatarian' },
-    { name: 'Fruit Salad', diet: 'vegan' },
-    { name: 'Tofu Stir-Fry', diet: 'vegan' },
-    { name: 'Baked Fish', diet: 'pescatarian' },
-    { name: 'Veggie Wrap', diet: 'vegetarian' },
-    { name: 'Omelette', diet: 'protein-rich' },
-    { name: 'Mango Salsa Chicken', diet: 'protein-rich' },
-    { name: 'Avocado Toast', diet: 'vegetarian' },
-    { name: 'Greek Salad', diet: 'vegetarian' },
-    { name: 'Shrimp Scampi', diet: 'pescatarian' },
-    { name: 'Eggplant Parmesan', diet: 'vegetarian' },
-    { name: 'Tuna Salad', diet: 'pescatarian' },
-    { name: 'Hummus Wrap', diet: 'vegan' },
-  ];
+  // const menuItems = [
+  //   { name: 'Burrito', diet: 'vegan' },
+  //   { name: 'Salad', diet: 'vegetarian' },
+  //   { name: 'Pasta', diet: 'gluten-free' },
+  //   { name: 'Smoothie', diet: 'vegan' },
+  //   { name: 'Grilled Chicken', diet: 'protein-rich' },
+  //   { name: 'Sushi Roll', diet: 'pescatarian' },
+  //   { name: 'Fruit Salad', diet: 'vegan' },
+  //   { name: 'Tofu Stir-Fry', diet: 'vegan' },
+  //   { name: 'Baked Fish', diet: 'pescatarian' },
+  //   { name: 'Veggie Wrap', diet: 'vegetarian' },
+  //   { name: 'Omelette', diet: 'protein-rich' },
+  //   { name: 'Mango Salsa Chicken', diet: 'protein-rich' },
+  //   { name: 'Avocado Toast', diet: 'vegetarian' },
+  //   { name: 'Greek Salad', diet: 'vegetarian' },
+  //   { name: 'Shrimp Scampi', diet: 'pescatarian' },
+  //   { name: 'Eggplant Parmesan', diet: 'vegetarian' },
+  //   { name: 'Tuna Salad', diet: 'pescatarian' },
+  //   { name: 'Hummus Wrap', diet: 'vegan' },
+  // ];
+
+  const [menus, setMenus] = useState([]);
+
+  useEffect(() => {
+    async function fetchMenus() {
+      const response = await fetch('http://localhost:8080/menu');
+      const data = await response.json();
+      setMenus(data);
+    }
+    fetchMenus();
+  }, []);
 
   return (
     <div className='w-4/5 grid grid-cols-5 gap-3'>
-      {menuItems.map((item, index) => (
-        <MenuCard key={index} name={item.name} diet={item.diet} />
+      {menus.map((menu) => (
+        <MenuCard key={menu.id} menu={menu} />
       ))}
     </div>
   );
