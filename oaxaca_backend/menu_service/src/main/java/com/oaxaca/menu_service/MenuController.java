bd941a04259c853550426da0a7a3f7171974a778
AUNG Htet (2023) WLIS379 <htet.aung.2023@live.rhul.ac.uk>
2024-02-15 00:00:05 +0000

    added cors configuration in menu controller

--- oaxaca_backend/menu_service/src/main/java/com/oaxaca/menu_service/MenuController.java
+++ oaxaca_backend/menu_service/src/main/java/com/oaxaca/menu_service/MenuController.java
@@ -2,13 +2,14 @@ package com.oaxaca.menu_service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class MenuController {
-
   @Autowired
   private MenuService menuService;
   
