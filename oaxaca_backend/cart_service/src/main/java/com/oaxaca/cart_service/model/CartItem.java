fba65020997886a7a6f7e0e0c34979ac6241d7e3
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-02-15 08:53:54 +0000

    Implement CartItem model
    To model the cart items, it was necessary to create a CartItem object

---
+++ oaxaca_backend/cart_service/src/main/java/com/oaxaca/cart_service/model/CartItem.java
@@ -0,0 +1,39 @@
+package com.oaxaca.cart_service.model;
+
+public class CartItem {
+
+    private int id;
+    private int productId;
+    private int quantity;
+
+    public CartItem(int id, int productId, int quantity) {
+        this.id = id;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+}
