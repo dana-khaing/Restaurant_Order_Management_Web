bf95d4804f9d3498c8676aed8add9e7d89b9ae49
AUNG Htet (2023) WLIS379 <htet.aung.2023@live.rhul.ac.uk>
2024-02-15 16:23:31 +0000

    added allergens and description in menu card

--- oaxaca/app/(home)/menus/components/menu-card.jsx
+++ oaxaca/app/(home)/menus/components/menu-card.jsx
@@ -1,12 +1,21 @@
-function MenuCard({ menu: { name, description, price } }) {
+function MenuCard({ menu: { name, description, price, allergens } }) {
   return (
     <div className='w-52 border border-[#EF3C3C] rounded-xl flex flex-col'>
       <img src='/images/burrito.jpeg' className='w-full h-36 rounded-t-xl' />
       <div className='p-2 flex-1 flex flex-col'>
         <h5 className='text-md font-semibold line-clamp-1'>{name}</h5>
-        <span className='text-xs px-1 bg-green-500 capitalize rounded-md line-clamp-2 my-3'>
-          {description}
-        </span>
+        <span className='text-gray-600 text-sm'>£{price}</span>
+
+        <div className='my-1'>
+          {allergens.map((allergen) => (
+            <span className='text-xs mr-1 p-1 bg-green-500 capitalize rounded-md'>
+              {allergen}
+            </span>
+          ))}
+        </div>
+
+        <p className='line-clamp-1 text-sm mb-2 text-gray-500'>{description}</p>
+
         <button className='mt-auto bg-[#EF3C3C] text-white rounded-lg px-7 py-1 block mx-auto'>
           Detail
         </button>
