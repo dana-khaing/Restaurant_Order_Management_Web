82d40be50764667bb10978195f2ab964858485c1
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-02-23 12:40:54 +0000

    Implement waiter functionality related to confirming orders, cancelling and sending orders to the kitchen. Fixed shared_library release-breaking bug

--- oaxaca_backend/shared_library/src/main/java/com/oaxaca/shared_library/model/order/IOrder.java
+++ oaxaca_backend/shared_library/src/main/java/com/oaxaca/shared_library/model/order/IOrder.java
@@ -2,13 +2,14 @@ package com.oaxaca.shared_library.model.order;
 
 import java.util.List;
 
+
 public interface IOrder {
 
     Long getId();
     void setId(Long id);
 
-    List<OrderItem> getOrderItems();
-    void setOrderItems(List<OrderItem> orderItems);
+    List<IOrderItem> getOrderItems();
+    void setOrderItems(List<IOrderItem> orderItems);
 
     OrderType getOrderType();
     void setOrderType(OrderType orderType);
--- oaxaca_backend/waiter_service/src/main/java/com/oaxaca/waiter_service/repository/OrderRepository.java
+++ oaxaca_backend/waiter_service/src/main/java/com/oaxaca/waiter_service/repository/OrderRepository.java
@@ -2,9 +2,9 @@ package com.oaxaca.waiter_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.oaxaca.shared_library.model.order.Order;
 import com.oaxaca.shared_library.model.order.OrderStatus;
 import com.oaxaca.shared_library.model.order.OrderType;
+import com.oaxaca.waiter_service.model.Order;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,7 +26,7 @@ public interface OrderRepository extends JpaRepository<Order, Long>{
 
     public List<Order> findByOrderStatusAndOrderType(OrderStatus orderStatus, OrderType orderType);
 
-    
+
     
     
 }
