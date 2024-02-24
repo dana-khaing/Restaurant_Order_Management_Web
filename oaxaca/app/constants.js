b605259a38a2efef623c7859be09ddd1e1ffbcb9
AUNG Htet (2023) WLIS379 <htet.aung.2023@live.rhul.ac.uk>
2024-02-24 11:04:13 +0000

    fixed port number for menu service

--- oaxaca/app/constants.js
+++ oaxaca/app/constants.js
@@ -2,5 +2,5 @@ export const SERVICE_URLS = {
   CUSTOMER_SERVICE: 'http://localhost:8080',
   KITCHEN_STAFF_SERVICE: 'http://localhost:8081',
   WAITER_SERVICE: 'http://localhost:8082',
-  MENU_SERVICE: 'http://localhost:8084',
+  MENU_SERVICE: 'http://localhost:8083',
 };
