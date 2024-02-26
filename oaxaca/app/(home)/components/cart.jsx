7735fe48b0e18ef5ac2d2bea253b8f24e7531570
AUNG Htet (2023) WLIS379 <htet.aung.2023@live.rhul.ac.uk>
2024-02-26 10:50:13 +0000

    added cart icon in the navbar

---
+++ oaxaca/app/(home)/components/cart.jsx
@@ -0,0 +1,13 @@
+import { ShoppingCart } from 'lucide-react';
+import React from 'react';
+
+export default function Cart() {
+  return (
+    <div className='relative mr-3'>
+      <span className='absolute -right-2 -top-2 inline-block h-5 w-5 rounded-full bg-gray-300 text-center text-xs font-medium leading-5 text-black'>
+        3
+      </span>
+      <ShoppingCart className='' size={'24px'} />
+    </div>
+  );
+}
