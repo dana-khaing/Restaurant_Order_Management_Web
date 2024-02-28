b869eed2d1da5bbbeba07259c8ca2974f7c9cd42
Michael Goodwin <ZLAC139@live.rhul.ac.uk>
2024-02-28 22:56:25 +0000

    Add Initial Spring Initializr Project
    Added initial project for the waiter summon service, generated by Spring Initializr.

---
+++ oaxaca_backend/waiter_summon_service/src/main/java/com/oaxaca/waiter_summon_service/WaiterSummonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oaxaca.waiter_summon_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaiterSummonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaiterSummonServiceApplication.class, args);
+	}
+
+}
