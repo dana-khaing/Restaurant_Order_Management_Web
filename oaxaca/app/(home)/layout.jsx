97d2b25f02891de736abd50a5bd98616a8f5a61b
Dana Khaing <WLIS410@live.rhul.ac.uk>
2024-02-29 12:44:09 +0000

    added the sample logo for project

--- oaxaca/app/(home)/layout.jsx
+++ oaxaca/app/(home)/layout.jsx
@@ -5,7 +5,13 @@ export default function HomeLayout({ children }) {
   return (
     <div>
       <div className="bg-[#EF3C3C] flex justify-between items-center p-4">
-        <span className="text-2xl text-white tracking-wider">OAXACA</span>
+        <span className="text-2xl text-white tracking-wider">
+          <img
+            src="images/logo_sample.png"
+            alt="logo"
+            className=" flex justify-center align-middle w-[12.5rem] h-11 "
+          />
+        </span>
         <div className="flex gap-3 items-center">
           <CallWaiterButton />
           <Link href="/customer/login">
