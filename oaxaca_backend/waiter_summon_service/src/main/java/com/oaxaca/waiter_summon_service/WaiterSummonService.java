b3aad92e9faf180a68e73d1fc456c5e040151be6
Michael Goodwin <ZLAC139@live.rhul.ac.uk>
2024-02-29 18:46:50 +0000

    Add database access methods to WaiterSummonService
    Added methods to WaiterSummonService for adding, updating and deleting WaiterSummonRequest records.

--- oaxaca_backend/waiter_summon_service/src/main/java/com/oaxaca/waiter_summon_service/WaiterSummonService.java
+++ oaxaca_backend/waiter_summon_service/src/main/java/com/oaxaca/waiter_summon_service/WaiterSummonService.java
@@ -1,5 +1,7 @@
 package com.oaxaca.waiter_summon_service;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +14,34 @@ public class WaiterSummonService {
   public WaiterSummonService(WaiterSummonRequestRepository waiterSummonRequestRepository) {
     this.waiterSummonRequestRepository = waiterSummonRequestRepository;
   }
+  
+  public List<WaiterSummonRequest> getAllWaiterSummonRequests() {
+    List<WaiterSummonRequest> allRequests = new ArrayList<>();
+    waiterSummonRequestRepository.findAll().forEach(allRequests::add);
+    return allRequests;
+  }
+  
+  public WaiterSummonRequest getWaiterSummonRequest(int id) {
+    return waiterSummonRequestRepository.findById(id).orElse(null);
+  }
+  
+  public void addWaiterSummonRequest(WaiterSummonRequest wsr) {
+    waiterSummonRequestRepository.save(wsr);
+  }
 
+  public void updateWaiterSummonRequest(WaiterSummonRequest wsr) {
+    waiterSummonRequestRepository.save(wsr);
+  }
+  
+  public void deleteWaiterSummonRequest(int id) {
+    waiterSummonRequestRepository.deleteById(id);
+  }
+  
+  public void markAsServed(int id) {
+    WaiterSummonRequest wsr = waiterSummonRequestRepository.findById(id).orElse(null);
+    if (wsr == null) {
+      return;
+    }
+    wsr.setIsCustomerServed(true);
+  }
 }
