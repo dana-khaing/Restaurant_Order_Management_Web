74bf4467b25bb86fb3b248f4428e99f9a289d0a9
Michael Goodwin <ZLAC139@live.rhul.ac.uk>
2024-03-05 14:07:49 +0000

    Complete integration of WaiterSummonService
    The methods and fields from waiter_summon_service have been added into waiter_service.

--- oaxaca_backend/waiter_service/src/main/java/com/oaxaca/waiter_service/repository/WaiterSummonRequestRepository.java
+++ oaxaca_backend/waiter_service/src/main/java/com/oaxaca/waiter_service/repository/WaiterSummonRequestRepository.java
@@ -1,5 +1,10 @@
 package com.oaxaca.waiter_service.repository;
 
-public class WaiterSummonRequestRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.oaxaca.waiter_service.model.WaiterSummonRequest;
+
+@Repository
+public interface WaiterSummonRequestRepository extends CrudRepository<WaiterSummonRequest, Integer> {
 
 }
--- oaxaca_backend/waiter_service/src/main/java/com/oaxaca/waiter_service/service/WaiterSummonService.java
+++ oaxaca_backend/waiter_service/src/main/java/com/oaxaca/waiter_service/service/WaiterSummonService.java
@@ -1,5 +1,49 @@
 package com.oaxaca.waiter_service.service;
 
-public class WaiterSummonService {
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.oaxaca.waiter_service.model.WaiterSummonRequest;
+import com.oaxaca.waiter_service.repository.WaiterSummonRequestRepository;
 
+@Service
+public class WaiterSummonService {
+  
+  private WaiterSummonRequestRepository waiterSummonRequestRepository;
+  
+  @Autowired
+  public WaiterSummonService(WaiterSummonRequestRepository waiterSummonRequestRepository) {
+    this.waiterSummonRequestRepository = waiterSummonRequestRepository;
+  }
+  
+  public List<WaiterSummonRequest> getAllWaiterSummonRequests() {
+    List<WaiterSummonRequest> allRequests = new ArrayList<>();
+    waiterSummonRequestRepository.findAll().forEach(allRequests::add);
+    return allRequests;
+  }
+  
+  public WaiterSummonRequest getWaiterSummonRequest(int id) {
+    return waiterSummonRequestRepository.findById(id).orElse(null);
+  }
+  
+  public void addWaiterSummonRequest(WaiterSummonRequest wsr) {
+    waiterSummonRequestRepository.save(wsr);
+  }
+
+  public void updateWaiterSummonRequest(WaiterSummonRequest wsr) {
+    waiterSummonRequestRepository.save(wsr);
+  }
+  
+  public void deleteWaiterSummonRequest(int id) {
+    waiterSummonRequestRepository.deleteById(id);
+  }
+  
+  public void markAsServed(int id) {
+    WaiterSummonRequest wsr = waiterSummonRequestRepository.findById(id).orElse(null);
+    if (wsr == null) {
+      return;
+    }
+    wsr.setIsCustomerServed(true);
+  }
 }
