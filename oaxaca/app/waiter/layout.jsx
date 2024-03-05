a448301deaa5311caa2339899c72217a554975fc
AUNG Htet (2023) WLIS379 <htet.aung.2023@live.rhul.ac.uk>
2024-03-05 22:55:03 +0000

    set up waiter layout

--- oaxaca/app/waiter/layout.jsx
+++ oaxaca/app/waiter/layout.jsx
@@ -1,27 +1,19 @@
-import CallWaiterButton from '../custom_components/call-watier-btn';
-import { NavList } from './components/nav_list';
-import Cart from './components/cart';
 import { UserRound } from 'lucide-react';
 import { fetchCart } from '../actions/cart';
 
-export default async function HomeLayout({ children }) {
+export default async function WaiterLayout({ children }) {
   const cart = await fetchCart();
   return (
     <div>
       <div className='bg-[#EF3C3C] text-white text-lg font-medium flex justify-between items-center p-4'>
         <a href='/'>
           <img
-            src='images/logo_sample.png'
+            src='/images/logo_sample.png'
             alt='logo'
             className=' flex justify-center align-middle w-[12.5rem] h-11 '
           />
         </a>
-        <div>
-          <NavList />
-        </div>
         <div className='flex gap-3 items-center'>
-          <CallWaiterButton />
-          <Cart cartItems={cart?.items} />
           <UserRound />
           <a href='/customer/login'>Login</a>
         </div>
