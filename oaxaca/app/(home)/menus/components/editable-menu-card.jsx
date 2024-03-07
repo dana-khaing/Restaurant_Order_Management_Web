7cbf26de8f1480d9e731924cb4728b215754c5e8
Irene Vazquez <ZLAC182@live.rhul.ac.uk>
2024-03-07 13:41:12 +0000

    handle input change in EditableMenuItem component

--- oaxaca/app/(home)/menus/components/editable-menu-card.jsx
+++ oaxaca/app/(home)/menus/components/editable-menu-card.jsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 const EditableMenuItem = ({ item, onSave, onCancel }) => {
     const [editedItem, setEditedItem] = useState(item);
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
-};
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setEditedItem((prevItem) => ({ ...prevItem, [name]: value }));
+      };
+    };
