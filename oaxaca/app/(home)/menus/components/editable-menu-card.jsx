093ff1198b7bd2c8212af5ce73dc3f79c3cfdb15
Irene Vazquez <ZLAC182@live.rhul.ac.uk>
2024-03-07 13:16:18 +0000

    add state and event handler for name input

--- oaxaca/app/(home)/menus/components/editable-menu-card.jsx
+++ oaxaca/app/(home)/menus/components/editable-menu-card.jsx
@@ -1,3 +1,10 @@
 import React, { useState } from 'react';
 
-const EditableMenuCard = ({ index, name: initialName, diet: initialDiet, onSave, onCancel }) => { }
+const EditableMenuCard = ({ index, name: initialName, diet: initialDiet, onSave, onCancel }) => {
+  const [name, setName] = useState(initialName);
+  const [diet, setDiet] = useState(initialDiet);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+};
