349bf6eefaf5b9df213be4bd03973e1c64da4beb
Michael Goodwin <ZLAC139@live.rhul.ac.uk>
2024-03-08 19:26:21 +0000

    Add Javadoc to MenuRepository class
    Added Javadoc to the MenuRepository class in the menu service.

--- oaxaca_backend/menu_service/src/main/java/com/oaxaca/menu_service/MenuRepository.java
+++ oaxaca_backend/menu_service/src/main/java/com/oaxaca/menu_service/MenuRepository.java
@@ -3,7 +3,13 @@ package com.oaxaca.menu_service;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * MenuRepository is a class that extends CrudRepository, which is an interface for database
+ * operations. This makes it possible to call methods for interacting with the database.
+ * 
+ * @author Michael Goodwin
+ */
 @Repository
 public interface MenuRepository extends CrudRepository<MenuItem, Integer> {
-  
+
 }
