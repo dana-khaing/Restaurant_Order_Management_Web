4bd94b2d661c04376d0a6e8ae44a2ded5e5e9849
Dana Khaing <WLIS410@live.rhul.ac.uk>
2024-03-12 22:49:20 +0000

    Added simple footer to the landing page

---
+++ oaxaca/components/ui/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4">
+      <p>&copy; 2021 OAXACA Restaurant</p>
+    </footer>
+  );
+}
