f9f15a1a8bfbaf8e3c041e579a9a7f02822be34d
Irene Vazquez <ZLAC182@live.rhul.ac.uk>
2024-03-12 21:10:48 +0000

    previous implementation deleted because it was wrong and start of new function created

--- oaxaca/app/(home)/menus/components/editable-menu-card.jsx
+++ oaxaca/app/(home)/menus/components/editable-menu-card.jsx
@@ -1,14 +1,3 @@
 import React, { useState } from 'react';
 
-const EditableMenuItem = ({ item, onSave, onCancel }) => {
-    const [editedItem, setEditedItem] = useState(item);
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setEditedItem((prevItem) => ({ ...prevItem, [name]: value }));
-      };
-
-    const handleSave = () => {
-        onSave(editedItem);
-    };
-    };
+function EditableMenuItem() { } 
