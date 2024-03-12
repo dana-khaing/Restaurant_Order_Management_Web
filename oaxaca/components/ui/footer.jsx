bee027360528cb76816f0234d6e5b64d0b311aff
Dana Khaing <WLIS410@live.rhul.ac.uk>
2024-03-12 23:26:15 +0000

    added social media icon in the footer

--- oaxaca/components/ui/footer.jsx
+++ oaxaca/components/ui/footer.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { Facebook } from "lucide-react";
+import { Linkedin } from "lucide-react";
+import { Twitter } from "lucide-react";
+import { Instagram } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white text-center py-4">
-      <p>&copy; 2021 OAXACA Restaurant</p>
+    <footer className="bg-[#EF3C3C] text-white text-center py-5 px-5">
+      <p>&copy; 2024 OAXACA Restaurant</p>
+
+      <div className="flex justify-evenly items-center h-10 ">
+        <a href="">
+          <Instagram size={40} />
+        </a>
+        <a href="">
+          <Facebook size={40} className="border-2 rounded-md px-[0.15rem]" />
+        </a>
+        <a href="">
+          <Linkedin size={40} className="border-2 rounded-md px-[0.15rem]" />
+        </a>
+        <a href="">
+          <Twitter size={40} />
+        </a>
+      </div>
     </footer>
   );
 }
