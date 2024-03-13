b4e5bdf85acab6971198f49c624e277df3046516
Irene Vazquez <ZLAC182@live.rhul.ac.uk>
2024-03-13 16:23:36 +0000

    added const to update the availability of the item

--- oaxaca/app/(home)/menus/components/editable-menu-card.jsx
+++ oaxaca/app/(home)/menus/components/editable-menu-card.jsx
@@ -3,17 +3,34 @@ import MenuCard from './menu-card';
 import { SERVICE_URLS } from '@/app/constants';
 
 function EditableMenuItem(item) { 
-  const response = fetch(`${SERVICE_URLS.MENU_SERVICE}/menu/${id}`, {
-    method: 'PUT',
-    headers: {
+    const { id, category, name, description, price, allergens, calories, availability } = item;
+    const response = fetch(`${SERVICE_URLS.MENU_SERVICE}/menu/${id}`, {
+      method: 'PUT',
+      headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(item),
   });
+  const updateItemAvailability = async () => {
+    // Clone item and update availability
+    const updatedItem = { ...item, availability: false };
 
+    try {
+      const response = await fetch(`${SERVICE_URLS.MENU_SERVICE}/menu/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updatedItem),
+      });
+    } catch (error) {
+      console.error("Failed to update item:", error);
+    }
+  };
   const [activeFilters, setActiveFilters] = useState({
     availability: false
 } );
+  
 const toggleFilter = (filter) => {
   setActiveFilters((prevFilters) => ({
     ...prevFilters,
@@ -21,3 +38,4 @@ const toggleFilter = (filter) => {
   }));
 }; 
 }
+
