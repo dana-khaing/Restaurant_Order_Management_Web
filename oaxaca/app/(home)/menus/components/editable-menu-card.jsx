23d43c15927cee4ea8db4cdc3e9eb562ff7e0b49
Irene Vazquez <ZLAC182@live.rhul.ac.uk>
2024-03-13 11:58:06 +0000

    added menuModal components

--- oaxaca/app/(home)/menus/components/editable-menu-card.jsx
+++ oaxaca/app/(home)/menus/components/editable-menu-card.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
+import MenuModal from './menu-modal';
+import { cn } from '@/lib/utils';
 
 function EditableMenuItem() { 
   const [activeFilters, setActiveFilters] = useState({
-    menuItem : false
+    main_course: false
 } );
 const toggleFilter = (filter) => {
   setActiveFilters((prevFilters) => ({
     ...prevFilters,
     [filter]: !prevFilters[filter],
   }));
-};
+}
 }
