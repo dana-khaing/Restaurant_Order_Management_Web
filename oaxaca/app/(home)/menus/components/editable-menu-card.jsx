593c26f6c85b4bd2091b474bf95391e2649b54d5
Irene Vazquez <ZLAC182@live.rhul.ac.uk>
2024-03-13 15:31:09 +0000

    conenction to the menu service stablished

--- oaxaca/app/(home)/menus/components/editable-menu-card.jsx
+++ oaxaca/app/(home)/menus/components/editable-menu-card.jsx
@@ -3,13 +3,15 @@ import MenuCard from './menu-card';
 import { SERVICE_URLS } from '@/app/constants';
 
 function EditableMenuItem() { 
+  const response = fetch(`${SERVICE_URLS.MENU_SERVICE}/menu/${id}`);
+
   const [activeFilters, setActiveFilters] = useState({
-    main_course: false
+    availability: false
 } );
 const toggleFilter = (filter) => {
   setActiveFilters((prevFilters) => ({
     ...prevFilters,
     [filter]: !prevFilters[filter],
   }));
-}
+}; 
 }
