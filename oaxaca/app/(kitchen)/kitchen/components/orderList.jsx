35c7afc0b9991376e72e9bba8bd7b74b41265bc5
Dana Khaing <WLIS410@live.rhul.ac.uk>
2024-03-14 20:39:54 +0000

    refactored the orderList

--- oaxaca/app/(kitchen)/kitchen/components/orderList.jsx
+++ oaxaca/app/(kitchen)/kitchen/components/orderList.jsx
@@ -3,11 +3,22 @@ import React from "react";
 export default function OrderList() {
   return (
     <div className="grid grid-cols-5">
-      <div className=" h-[100%] w-[100%] border-2 border-[#786c6c] flex-col m-4 p-4 justify-center items-center">
-        <h1 className=" text-center">Table 17</h1>
+      <div className="  w-[100%] border-2 border-[#786c6c] flex-col m-4 p-4 items-center">
+        <h1 className=" text-center underline pb-4 font-semibold text-[#5D5252]">
+          Table 17
+        </h1>
         <div>Burrito</div>
         <div>Bowl</div>
         <div>Quesadilla</div>
+        <div className="flex justify-center align-middle items-center m-5">
+          <button className=" bg-[#5D5252] text-white text-center p-3 rounded-[0.8rem] ">
+            Bump Order
+          </button>
+        </div>
+        <div className="flex justify-end text-sm gap-2 ">
+          <p>Promise Time :</p>
+          <p> 00 : 00 </p>
+        </div>
       </div>
     </div>
   );
