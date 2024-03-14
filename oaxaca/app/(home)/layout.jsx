1d250e5dfd2efc84aa3093917189eb5e6a271ce4
AUNG Htet (2023) WLIS379 <htet.aung.2023@live.rhul.ac.uk>
2024-03-14 20:04:26 +0000

    merge conflict resolved

--- oaxaca/app/(home)/layout.jsx
+++ oaxaca/app/(home)/layout.jsx
@@ -1,39 +1,31 @@
-<<<<<<< HEAD
-import Link from "next/link";
-import CallWaiterButton from "../custom_components/call-watier-btn";
-import { NavList } from "./components/nav_list";
-import Cart from "./components/cart";
-import { UserRound } from "lucide-react";
-import { fetchCart } from "../actions/cart";
-import Footer from "./components/footer";
-=======
+import Link from 'next/link';
 import CallWaiterButton from '../custom_components/call-watier-btn';
 import { NavList } from './components/nav_list';
 import Cart from './components/cart';
 import { UserRound } from 'lucide-react';
 import { fetchCart } from '../actions/cart';
->>>>>>> feature/waiter-ui
+import Footer from './components/footer';
 
 export default async function HomeLayout({ children }) {
   const cart = await fetchCart();
   return (
     <div>
-      <div className="bg-[#EF3C3C] text-white text-lg font-medium flex justify-between items-center p-4">
-        <a href="/">
+      <div className='bg-[#EF3C3C] text-white text-lg font-medium flex justify-between items-center p-4'>
+        <a href='/'>
           <img
-            src="images/logo_sample.png"
-            alt="logo"
-            className=" flex justify-center align-middle w-[10rem] h-[2.5rem] "
+            src='images/logo_sample.png'
+            alt='logo'
+            className=' flex justify-center align-middle w-[10rem] h-[2.5rem] '
           />
         </a>
         <div>
           <NavList />
         </div>
-        <div className="flex gap-3 items-center">
+        <div className='flex gap-3 items-center'>
           <CallWaiterButton />
           <Cart cartItems={cart?.items} />
           <UserRound />
-          <a href="/customer/login">Login</a>
+          <a href='/customer/login'>Login</a>
         </div>
       </div>
       {children}
