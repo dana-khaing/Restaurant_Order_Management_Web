45e9b5998f62a58b4221380ea565c818a11531a6
Michael Goodwin <ZLAC139@live.rhul.ac.uk>
2024-03-15 02:48:15 +0000

    Add images to project folder
    Imgur did not reliably return the images, so the images have been added to directly to the project folder.
    
    The imageURL field for each item in the demo menu has been altered to reflect this.

--- oaxaca_backend/menu_service/src/main/java/com/oaxaca/menu_service/MenuDatabaseInitialiser.java
+++ oaxaca_backend/menu_service/src/main/java/com/oaxaca/menu_service/MenuDatabaseInitialiser.java
@@ -33,100 +33,100 @@ public class MenuDatabaseInitialiser implements CommandLineRunner {
    */
   private MenuItem item1 = new MenuItem(1, 1, "Tlayudas (Vegetarian option)",
       "Large, thin, crispy tortillas with vegetable toppings.", 6.50f, Arrays.asList("Gluten"), 528,
-      true, "https://i.imgur.com/RFKBd0u.jpg");
+      true, "/images/tlayudas-veg.jpg");
   /**
    * Menu item 2 of demo menu: Tostadas de Chapulines
    */
   private MenuItem item2 = new MenuItem(2, 1, "Tostadas de Chapulines",
       "Tostadas with guacamole and seasoned chapulines.", 7.99f, Arrays.asList("Shellfish"), 315,
-      true, "https://i.imgur.com/5H51tjZ.jpg");
+      true, "/images/tostadas-de-chapulines.jpg");
   /**
    * Menu item 3 of demo menu: Guacamole
    */
   private MenuItem item3 = new MenuItem(3, 1, "Guacamole",
       "Fresh avocados, tomatoes, onions, cilantro, lime juice, salt, and pepper.", 6.50f,
-      Arrays.asList("None"), 256, true, "https://i.imgur.com/yrThUBQ.jpg");
+      Arrays.asList("None"), 256, true, "/images/guacamole.jpg");
   /**
    * Menu item 4 of demo menu: Mole Negro with Chicken
    */
   private MenuItem item4 =
       new MenuItem(4, 2, "Mole Negro with Chicken", "Slow-cooked chicken in dark mole sauce.",
-          11.99f, Arrays.asList("Nuts", "Gluten"), 681, true, "https://imgur.com/a/jCFy2AI");
+          11.99f, Arrays.asList("Nuts", "Gluten"), 681, true, "/images/mole-negro-with-chicken.jpg");
   /**
    * Menu item 5 of demo menu: Shrimp ceviche
    */
   private MenuItem item5 = new MenuItem(5, 2, "Shrimp ceviche",
       "Shrimp marinated in lime juice, mixed with tomatoes, onions, cilantro, and avocado.", 12.99f,
-      Arrays.asList("Shellfish"), 421, true, "https://i.imgur.com/FdQuLGD.jpg");
+      Arrays.asList("Shellfish"), 421, true, "/images/shrimp-ceviche.jpg");
   /**
    * Menu item 6 of demo menu: Tacos al pastor
    */
   private MenuItem item6 = new MenuItem(6, 2, "Tacos al pastor",
       "Marinated and grilled pork served in corn tortillas with pineapple and cilantro.", 9.99f,
-      Arrays.asList("Gluten"), 553, true, "https://i.imgur.com/62TifLV.jpg");
+      Arrays.asList("Gluten"), 553, true, "/images/tacos-al-pastor.jpg");
   /**
    * Menu item 7 of demo menu: Tlayudas con Tasajo
    */
   private MenuItem item7 = new MenuItem(7, 2, "Tlayudas con Tasajo",
       "Grilled and seasoned thinly sliced beef on tlayudas.", 12.99f, Arrays.asList("Gluten"), 602,
-      true, "https://i.imgur.com/w1fIP2V.jpg");
+      true, "/images/tlayudas-con-tasajo.jpg");
   /**
    * Menu item 8 of demo menu: Enfrijoladas
    */
   private MenuItem item8 = new MenuItem(8, 2, "Enfrijoladas",
       "Rolled tortillas filled with chicken and black bean sauce.", 11.99f, Arrays.asList("Gluten"),
-      499, true, "https://i.imgur.com/p7jgTYk.jpg");
+      499, true, "/images/enfrijoladas.jpg");
   /**
    * Menu item 9 of demo menu: Chiles Rellenos de Quesillo
    */
   private MenuItem item9 = new MenuItem(9, 3, "Chiles Rellenos de Quesillo",
       "Poblano peppers stuffed with Oaxacan cheese.", 10.99f, Arrays.asList("Dairy"), 457, true,
-      "https://i.imgur.com/pbrI8Yq.jpg");
+      "/images/chiles-rellenos-de-quesillo.jpg");
   /**
    * Menu item 10 of demo menu: Sikil P''ak
    */
   private MenuItem item10 = new MenuItem(10, 3, "Sikil P''ak (Pumpkin Seed Dip)",
       "Dip made with pumpkin seeds, tomatoes, and spices.", 8.99f, Arrays.asList(), 351, true,
-      "https://i.imgur.com/58dkmUd.jpg");
+      "/images/sikil-p-ak.jpg");
   /**
    * Menu item 11 of demo menu: Chocolate Oaxaqueño
    */
   private MenuItem item11 =
       new MenuItem(11, 4, "Chocolate Oaxaqueño", "Oaxacan-style hot chocolate.", 6.99f,
-          Arrays.asList("Dairy"), 292, true, "https://i.imgur.com/JX6oK5v.jpg");
+          Arrays.asList("Dairy"), 292, true, "/images/chocolate-oaxaqueno.jpg");
   /**
    * Menu item 12 of demo menu: Nicuatole
    */
   private MenuItem item12 =
       new MenuItem(12, 4, "Nicuatole", "Traditional Oaxacan dessert with masa and chocolate.",
-          7.99f, Arrays.asList("Dairy"), 330, true, "https://i.imgur.com/t9RwmN9.jpg");
+          7.99f, Arrays.asList("Dairy"), 330, true, "/images/nicuatole.jpg");
   /**
    * Menu item 13 of demo menu: Tres Leches Cake
    */
   private MenuItem item13 = new MenuItem(13, 4, "Tres Leches Cake",
       "Sponge cake soaked in three kinds of milk, topped with whipped cream and fresh berries.",
-      8.99f, Arrays.asList("Dairy", "Gluten"), 585, true, "https://i.imgur.com/SfLe62k.jpg");
+      8.99f, Arrays.asList("Dairy", "Gluten"), 585, true, "/images/tres-leches-cake.jpg");
   /**
    * Menu item 14 of demo menu: Mango Sorbet
    */
   private MenuItem item14 = new MenuItem(14, 4, "Mango Sorbet", "Refreshing mango sorbet.", 5.99f,
-      Arrays.asList("None"), 201, true, "https://i.imgur.com/Xk55Nxd.jpg");
+      Arrays.asList("None"), 201, true, "/images/mango-sorbet.jpg");
   /**
    * Menu item 15 of demo menu: Mezcal Cocktail
    */
   private MenuItem item15 =
       new MenuItem(15, 5, "Mezcal Cocktail", "Various mezcal-based cocktails.", 9.99f,
-          Arrays.asList("None"), 253, true, "https://i.imgur.com/4EOAui8.jpg");
+          Arrays.asList("None"), 253, true, "/images/mezcal-cocktail.jpg");
   /**
    * Menu item 16 of demo menu: Horchata
    */
   private MenuItem item16 = new MenuItem(16, 5, "Horchata", "Rice and based horchata.", 4.99f,
-      Arrays.asList("None"), 139, true, "https://i.imgur.com/mfaDzvs.jpg");
+      Arrays.asList("None"), 139, true, "/images/horchata.jpg");
   /**
    * Menu item 17 of demo menu: Coca-Cola
    */
   private MenuItem item17 = new MenuItem(17, 5, "Coca-Cola", "Carbonated soft drink with ice",
-      2.80f, Arrays.asList("None"), 210, true, "https://i.imgur.com/zYCTtjY.jpg");
+      2.80f, Arrays.asList("None"), 210, true, "/images/coca-cola.jpg");
 
   /** Initialises the database upon startup of the Spring Boot application if it is empty. */
   @Override
