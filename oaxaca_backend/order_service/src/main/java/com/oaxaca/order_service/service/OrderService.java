a40b43eb4389aafdbc8f59adf2c8f1d7c92b2134
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-03-18 16:28:15 +0000

    Migrate other order methods from waiter service

--- oaxaca_backend/order_service/src/main/java/com/oaxaca/order_service/service/OrderService.java
+++ oaxaca_backend/order_service/src/main/java/com/oaxaca/order_service/service/OrderService.java
@@ -1,14 +1,14 @@
 package com.oaxaca.order_service.service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.aspectj.internal.lang.annotation.ajcDeclareAnnotation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.oaxaca.order_service.dto.CartDto;
 import com.oaxaca.order_service.dto.CartItemDto;
 import com.oaxaca.order_service.dto.OrderDetailsDto;
 import com.oaxaca.order_service.model.Order;
@@ -46,6 +46,78 @@ public class OrderService {
         order.setOrderItems(orderItems);
 
         return orderRepository.save(order);
+    }
+
+    public void cancelOrder(Long orderId) {
+
+        if (orderId == null) {
+            throw new IllegalArgumentException("Order id cannot be null");
+        }
+
+        Optional<Order> order = orderRepository.findById(orderId);
+
+        if (!order.isPresent()) {
+            throw new IllegalArgumentException("Order not found");
+        }
+
+        orderRepository.deleteById(orderId);
+
+    }
+
+    public void completeOrder(Long orderId) {
+
+        if (orderId == null) {
+            throw new IllegalArgumentException("Order id cannot be null");
+        }
+
+        Optional<Order> order = orderRepository.findById(orderId);
+
+        if (!order.isPresent()) {
+            throw new IllegalArgumentException("Order not found");
+        }
+
+        Order orderToComplete = order.get();
+        orderToComplete.setOrderStatus(OrderStatus.COMPLETED);
+        orderRepository.save(orderToComplete);
+    }
+
+    public Order sendOrderToKitchen(Long orderId) {
+
+        if (orderId == null) {
+            throw new IllegalArgumentException("Order id cannot be null");
+        }
+
+        Optional<Order> order = orderRepository.findById(orderId);
+
+        if (!order.isPresent()) {
+            throw new IllegalArgumentException("Order not found");
+        }
+
+        Order orderToSend = order.get();
+        orderToSend.setOrderStatus(OrderStatus.IN_PROGRESS);
+        return orderRepository.save(orderToSend);
+    }
+
+    public void saveOrder(Order order) {
+        if (order == null) {
+            throw new IllegalArgumentException("Order cannot be null");
+        }
+
+        orderRepository.save(order);
+    }
+
+    public Page<Order> getAllOrders(Pageable pageable) {
+        return orderRepository.findAllByOrderByCreationDateDesc(pageable);
+
+    }
+
+    public Page<Order> getOrdersByStatus(OrderStatus orderStatus, Pageable pageable) {
+
+        if (orderStatus == null || pageable == null) {
+            throw new IllegalArgumentException("Order status and pageable cannot be null");
+        }
+
+        return orderRepository.findByOrderStatus(orderStatus, pageable);
     }
 
     private OrderItem convertToOrderItem(CartItemDto cartDto) {
