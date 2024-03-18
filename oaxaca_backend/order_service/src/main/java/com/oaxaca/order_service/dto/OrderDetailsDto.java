c82f7e41829793d0e1f4c4bb8017d0bb07c3678c
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-03-18 17:17:16 +0000

    Implement web socket tests

--- oaxaca_backend/order_service/src/main/java/com/oaxaca/order_service/dto/OrderDetailsDto.java
+++ oaxaca_backend/order_service/src/main/java/com/oaxaca/order_service/dto/OrderDetailsDto.java
@@ -17,6 +17,10 @@ public class OrderDetailsDto {
         this.orderType = OrderType.valueOf(orderType.toUpperCase());
     }
 
+    public OrderDetailsDto() {
+       
+    }
+
     public String getCustomerName() {
         return customerName;
     }
