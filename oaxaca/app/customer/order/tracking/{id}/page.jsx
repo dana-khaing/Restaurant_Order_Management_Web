5fb4874f999303269ad2b2688acb7725595b2d9b
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-03-19 14:46:31 +0000

    Implement route handler to place order

---
+++ oaxaca/app/customer/order/tracking/{id}/page.jsx
@@ -0,0 +1,9 @@
+
+export default function TrackOrderPage({params}){
+    return (
+        <div>
+            <h1>Track Order</h1>
+            <p>Order ID: {params.id}</p>
+        </div>
+    );
+}
