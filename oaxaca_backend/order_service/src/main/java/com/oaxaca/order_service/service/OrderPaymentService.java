dbf518b5a2bbe4af0cf13f38e366d0916a3d3dc5
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-03-19 17:02:26 +0000

    Integrate Stripe payment gateway

--- oaxaca_backend/order_service/src/main/java/com/oaxaca/order_service/service/OrderPaymentService.java
+++ oaxaca_backend/order_service/src/main/java/com/oaxaca/order_service/service/OrderPaymentService.java
@@ -1,5 +1,9 @@
 package com.oaxaca.order_service.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
@@ -7,12 +11,18 @@ import com.oaxaca.order_service.event.OrderPaidEvent;
 import com.oaxaca.order_service.model.Order;
 import com.oaxaca.order_service.repository.OrderRepository;
 import com.oaxaca.shared_library.model.order.OrderStatus;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
 
 @Service
 public class OrderPaymentService {
 
     private final OrderRepository orderRepository;
     private final ApplicationEventPublisher applicationEventPublisher;
+
+    @Value("${stripe.api.key}")
+    private String stripeApiKey;
 
     public OrderPaymentService(OrderRepository orderRepository, ApplicationEventPublisher applicationEventPublisher) {
         this.orderRepository = orderRepository;
@@ -35,11 +45,33 @@ public class OrderPaymentService {
             return false;
         }
 
-        order.setOrderStatus(OrderStatus.PAID);
-        orderRepository.save(order);
-        applicationEventPublisher.publishEvent(new OrderPaidEvent(this, order.getId()));
-        
-        return true;
+        Order orderToPay = orderRepository.findById(orderId).orElse(null);
+
+        if (orderToPay == null) {
+            return false;
+        }
+
+        Map<String, Object> chargeParams = new HashMap<>();
+        chargeParams.put("amount", orderToPay.getTotal());
+        chargeParams.put("currency", "gbp");
+        chargeParams.put("description", "Order payment");
+        chargeParams.put("source", "tok_visa");
+
+        try {
+            Charge charge = Charge.create(chargeParams);
+
+            if (charge.getPaid()) {
+                order.setOrderStatus(OrderStatus.PAID);
+                orderRepository.save(order);
+                applicationEventPublisher.publishEvent(new OrderPaidEvent(this, order.getId()));
+
+                return true;
+            }
+        } catch (StripeException e) {
+            return false;
+        }
+
+        return false;
 
     }
 
