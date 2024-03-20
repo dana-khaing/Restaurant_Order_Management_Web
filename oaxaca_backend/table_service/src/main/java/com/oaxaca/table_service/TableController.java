a88b35a4b194f7f724872f845aa0f8dab4d2c286
Michael Goodwin <ZLAC139@live.rhul.ac.uk>
2024-03-20 23:52:09 +0000

    Add TableController class to table service
    Added empty TableController class into the table service.

---
+++ oaxaca_backend/table_service/src/main/java/com/oaxaca/table_service/TableController.java
@@ -0,0 +1,10 @@
+package com.oaxaca.table_service;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TableController {
+
+}
