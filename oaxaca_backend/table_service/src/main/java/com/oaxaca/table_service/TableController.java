5799324205bee8ba045041bccbc9e0bf08e83d98
Michael Goodwin <ZLAC139@live.rhul.ac.uk>
2024-03-21 01:10:43 +0000

    Alter TableController stereotype to RestController
    Service encountered errors due to it being annotated with @Controller instead of @RestController.

--- oaxaca_backend/table_service/src/main/java/com/oaxaca/table_service/TableController.java
+++ oaxaca_backend/table_service/src/main/java/com/oaxaca/table_service/TableController.java
@@ -2,14 +2,14 @@ package com.oaxaca.table_service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TableController {
 
   @Autowired
@@ -34,4 +34,5 @@ public class TableController {
   public void deleteTable(@RequestBody int tableNumber) {
     tableService.deleteTable(tableNumber);
   }
+  
 }
