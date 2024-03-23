38f209924b4f798ad579f1d3d0a8f760ba3e1fc3
AUNG Htet (2023) WLIS379 <htet.aung.2023@live.rhul.ac.uk>
2024-03-23 20:30:46 +0000

    handled logout

--- oaxaca/app/(home)/components/user-profile.jsx
+++ oaxaca/app/(home)/components/user-profile.jsx
@@ -6,9 +6,17 @@ import { useContext } from 'react';
 import Cart from './cart';
 import CallWaiterButton from '@/app/custom_components/call-watier-btn';
 import Link from 'next/link';
+import { SERVICE_URLS } from '@/app/constants';
 
 export default function UserProfile({ cartItems }) {
-  const { user, loginUser, logoutUser } = useContext(AuthContext);
+  const { user, logoutUser } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    fetch(`/api/auth/customer/logout`).then((res) => {
+      console.log(res);
+      logoutUser();
+    });
+  };
 
   return (
     <div className='flex gap-3 items-center'>
@@ -19,7 +27,7 @@ export default function UserProfile({ cartItems }) {
           <h1>{user.username}</h1>
           <button
             className='bg-white text-red-500 px-3 py-2 rounded-lg'
-            onClick={logoutUser}
+            onClick={handleLogout}
           >
             Logout
           </button>
