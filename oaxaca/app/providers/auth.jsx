5649f75af67e8e8f06b5b28bd86253206ac25564
AUNG Htet (2023) WLIS379 <htet.aung.2023@live.rhul.ac.uk>
2024-03-23 12:26:40 +0000

    started implementing authcontext

---
+++ oaxaca/app/providers/auth.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { createContext, useState } from 'react';
+
+export const AuthContext = createContext({
+  user: null,
+  loginUser: (user) => {},
+});
+
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  const loginUser = (user) => setUser(user);
+  const logoutUser = () => setUser(null);
+
+  return (
+    <AuthContext.Provider value={(user, loginUser, logoutUser)}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
