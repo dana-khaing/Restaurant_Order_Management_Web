040674949b38d0f900a811a444b0460f523d417d
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-03-24 14:42:13 +0000

    Change success redirect url for oauth login to be dynamic

--- oaxaca/app/customer/order/tracking/{id}/page.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function TrackOrderPage({params}){
-    return (
-        <div>
-            <h1>Track Order</h1>
-            <p>Order ID: {params.id}</p>
-        </div>
-    );
-}
